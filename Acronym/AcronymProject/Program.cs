ed7717b7ef92e5f32c1941ba265b4f6abdd16293
Abraham Bergerson <AbrahamBergerson@gmail.com>
2020-02-28 09:50:31 -0600

    Created Acronym Project

---
+++ Acronym/AcronymProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AcronymProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please enter a series of words to create an acronym: ");
+            string acronym = CreateAcronym(Console.ReadLine());
+            Console.WriteLine("Your acronym: " + acronym.ToUpper());
+        }
+
+        static string CreateAcronym(string acr)
+        {
+            string phrase = "";
+            string[] newPhrase = acr.Split(' ');
+            foreach (string s in newPhrase)
+            {
+                phrase += s.Substring(0,  1);
+            }
+            return phrase;
+        }
+    }
+}
