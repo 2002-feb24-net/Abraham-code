ff978c78bc9a037406ec426600fde47a9720314d
Abraham Bergerson <AbrahamBergerson@gmail.com>
2020-03-02 14:12:44 -0600

    .

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,45 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pop pop fizz fizz oh what a relief it is!");
+            FizzingBuzzing();
+        }
+        
+        static void FizzingBuzzing()
+        {
+            int numCount = 0;
+            int fizzCount = 0;
+            int buzzCount = 0;
+            int fbCount = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    fbCount++;
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    fizzCount++;
+                    Console.WriteLine("Fizz");
+                }
+                else  if (i % 5 == 0)
+                {
+                    buzzCount++;
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    numCount++;
+                    Console.WriteLine(i.ToString());
+                }
+            }
+
+            Console.WriteLine("Fizz count = " + fizzCount.ToString());
+            Console.WriteLine("Buzz count = " + buzzCount.ToString());
+            Console.WriteLine("Fizzbuzz count = " + fbCount.ToString());
+            Console.WriteLine("Number count = " + numCount.ToString());
         }
     }
 }
